a6b35361f20ac7bf41ce9116892512b1b21771f4
StefanoSossi <tenosossi2012@gmail.com>
2023-06-03 11:50:39 -0400

    [Task-1] Format documents

--- src/Components/Calculator/calculator.jsx
+++ src/Components/Calculator/calculator.jsx
@@ -1,164 +1,187 @@
-import React, { useEffect, useState, useRef  } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./style.css";
 import axios from "axios";
 import evalOperation from "./eval";
 
 const Calculator = () => {
-  const [result, setResult] = useState("");
-  const [historial, setHistorial] = useState(null);
-  const listRef = useRef(null);
+	const [result, setResult] = useState("");
+	const [historial, setHistorial] = useState(null);
+	const listRef = useRef(null);
 
-  useEffect(()=> {
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get("historial.json");
+				setHistorial(response.data.history);
+			} catch (e) {
+				console.log(e);
+			}
+		};
 
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("historial.json");
-        setHistorial(response.data.history);
-      } catch (e) {
-        console.log(e);
-      }
-    };
+		fetchData();
+	}, []);
 
-    fetchData();
-  }, []);
+	useEffect(() => {
+		if (listRef.current && listRef.current.lastElementChild) {
+			listRef.current.lastElementChild.scrollIntoView({ behavior: "smooth" });
+		}
+	}, [historial]);
 
-  useEffect(() => {
-    if (listRef.current && listRef.current.lastElementChild) {
-        listRef.current.lastElementChild.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [historial]);
+	const calculateHandler = () => {
+		if (errorHandler()) {
+			const evalResult = evalOperation(result);
+			const newResult = {
+				id: historial.length + 1,
+				operation: result,
+				result: evalResult,
+			};
+			setResult(evalResult);
+			setHistorial([...historial, newResult]);
+			console.log(historial);
+		} else {
+			setResult("");
+		}
+	};
 
+	function errorHandler() {
+		if (result === "") {
+			alert("Operation can not be empty");
+			return false;
+		}
 
-  const calculateHandler = () => {
-    if( errorHandler() ){
-        const evalResult = evalOperation(result);
-        const newResult = {
-            id: historial.length + 1,
-            operation: result,
-            result: evalResult
-        }
-        setResult(evalResult);
-        setHistorial([...historial, newResult]);
-        console.log(historial)
-    } else{
-      setResult('')
-    }
-  };
+		if (["/", "*"].includes(result[0])) {
+			alert("Operation can not start with an operator");
+			return false;
+		}
 
-  function errorHandler() {
-    if( result === ""){
-      alert("Operation can not be empty")
-      return false
-    }
+		if (result.includes("/0")) {
+			alert("Operation can not divide by 0");
+			return false;
+		}
 
-    if(["/","*"].includes(result[0])){
-      alert("Operation can not start with an operator")
-      return false
-    } 
+		if (/^[a-z,A-Z]$/.test(result[0])) {
+			alert("Operation can only have numbers");
+			return false;
+		}
 
-    if(result.includes("/0")){
-      alert("Operation can not divide by 0")
-      return false
-    }
+		const operatorsCombined = [
+			"-+",
+			"-*",
+			"-/",
+			"+-",
+			"+*",
+			"+/",
+			"*-",
+			"*+",
+			"*/",
+			"/-",
+			"/+",
+			"/*",
+		];
+		if (
+			operatorsCombined.some((operatorCombined) =>
+				result.includes(operatorCombined)
+			)
+		) {
+			alert("Operation can not have two consecutive operators");
+			return false;
+		}
 
-    if(/^[a-z,A-Z]$/.test(result[0])){
-      alert("Operation can only have numbers")
-      return false
-    } 
-    
-    const operatorsCombined = ['-+','-*','-/','+-','+*','+/','*-','*+','*/','/-','/+','/*'];
-    if(operatorsCombined.some(operatorCombined => result.includes(operatorCombined))){
-      alert("Operation can not have two consecutive operators")
-      return false
-    } 
+		if (result[0] === "+") {
+			result.splice(0, 1);
+		}
 
-    if(result[0] === '+'){
-      result.splice(0, 1);
-    } 
+		if (result[0] === "-") {
+			setResult(["0", ...result]);
+		}
 
-    if(result[0] === '-'){
-      setResult(['0',...result]);
-    }
+		return true;
+	}
 
-    return true
-  }
+	const deleteHandler = () => {
+		setResult(result.slice(0, -1));
+	};
 
-  const deleteHandler = () => {
-    setResult(result.slice(0,-1));
-  };
+	const typeHandler = (value) => {
+		setResult((prevResult) => prevResult + value);
+	};
 
-  const typeHandler = (value) => {
-    setResult((prevResult) => prevResult + value);
-  };
+	if (!historial) {
+		return <div>Loading...</div>;
+	}
 
-  if (!historial) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="container color-black">
-      <ul  ref={listRef} className="historial" style={{ listStyle: 'none', overflowY: 'auto' }}>
-        {historial.map((elem)=>(
-            <li key={elem.id}>
-                {elem.operation} = {elem.result} 
-            </li >
-        ))
-        }
-      </ul>
-      <div className="display">{result}</div>
-      <div className="button clear" onClick={() => { setResult("") }}>
-        CLEAR
-      </div>
-      <div className="button" onClick={() => typeHandler("7")}>
-        7
-      </div>
-      <div className="button" onClick={() => typeHandler("8")}>
-        8
-      </div>
-      <div className="button" onClick={() => typeHandler("9")}>
-        9
-      </div>
-      <div className="button" onClick={() => typeHandler("+")}>
-        +
-      </div>
-      <div className="button" onClick={() => typeHandler("4")}>
-        4
-      </div>
-      <div className="button" onClick={() => typeHandler("5")}>
-        5
-      </div>
-      <div className="button" onClick={() => typeHandler("6")}>
-        6
-      </div>
-      <div className="button" onClick={() => typeHandler("-")}>
-        -
-      </div>
-      <div className="button" onClick={() => typeHandler("1")}>
-        1
-      </div>
-      <div className="button" onClick={() => typeHandler("2")}>
-        2
-      </div>
-      <div className="button" onClick={() => typeHandler("3")}>
-        3
-      </div>
-      <div className="button" onClick={() => typeHandler("*")}>
-        *
-      </div>
-      <div className="button" onClick={() => typeHandler("0")}>
-        0
-      </div>
-      <div className="button" onClick={calculateHandler}>
-        =
-      </div>
-      <div className="button" onClick={() => typeHandler("/")}>
-        /
-      </div>
-      <div className="button" onClick={deleteHandler}>
-        DEL
-      </div>
-    </div>
-  );
+	return (
+		<div className="container color-black">
+			<ul
+				ref={listRef}
+				className="historial"
+				style={{ listStyle: "none", overflowY: "auto" }}
+			>
+				{historial.map((elem) => (
+					<li key={elem.id}>
+						{elem.operation} = {elem.result}
+					</li>
+				))}
+			</ul>
+			<div className="display">{result}</div>
+			<div
+				className="button clear"
+				onClick={() => {
+					setResult("");
+				}}
+			>
+				CLEAR
+			</div>
+			<div className="button" onClick={() => typeHandler("7")}>
+				7
+			</div>
+			<div className="button" onClick={() => typeHandler("8")}>
+				8
+			</div>
+			<div className="button" onClick={() => typeHandler("9")}>
+				9
+			</div>
+			<div className="button" onClick={() => typeHandler("+")}>
+				+
+			</div>
+			<div className="button" onClick={() => typeHandler("4")}>
+				4
+			</div>
+			<div className="button" onClick={() => typeHandler("5")}>
+				5
+			</div>
+			<div className="button" onClick={() => typeHandler("6")}>
+				6
+			</div>
+			<div className="button" onClick={() => typeHandler("-")}>
+				-
+			</div>
+			<div className="button" onClick={() => typeHandler("1")}>
+				1
+			</div>
+			<div className="button" onClick={() => typeHandler("2")}>
+				2
+			</div>
+			<div className="button" onClick={() => typeHandler("3")}>
+				3
+			</div>
+			<div className="button" onClick={() => typeHandler("*")}>
+				*
+			</div>
+			<div className="button" onClick={() => typeHandler("0")}>
+				0
+			</div>
+			<div className="button" onClick={calculateHandler}>
+				=
+			</div>
+			<div className="button" onClick={() => typeHandler("/")}>
+				/
+			</div>
+			<div className="button" onClick={deleteHandler}>
+				DEL
+			</div>
+		</div>
+	);
 };
 
 export default Calculator;
--- src/Components/Calculator/eval.js
+++ src/Components/Calculator/eval.js
@@ -1,57 +1,56 @@
-function evalOperation ( operation ) {
-    let operands = operation.split(/[-+/*]/);
-    let operators = operation.match(/[-+/*]/g);
+function evalOperation(operation) {
+	let operands = operation.split(/[-+/*]/);
+	let operators = operation.match(/[-+/*]/g);
 
-    if(!operators){
-        return operation
-    }
-    else{
-        let result = 0;
-        do {
-            [operators, operands, result] = evalMultDiv(operators, operands, result)
-        } while (operators.some((element) => ["/","*"].includes(element)));
-            
-        return String(evalSumSubst(operators, operands, result))
-    }
-};
+	if (!operators) {
+		return operation;
+	} else {
+		let result = 0;
+		do {
+			[operators, operands, result] = evalMultDiv(operators, operands, result);
+		} while (operators.some((element) => ["/", "*"].includes(element)));
 
-function evalMultDiv(operators, operands, result){
-    for (let i = 0; i < operators.length; i++) {
-        result = parseFloat(operands[i]);
-        const operator = operators[i];
-        const operand = parseFloat(operands[i + 1]);
-
-        if (operator === '*') {
-            result *= operand;
-            operands[i] = String(result)
-            operands.splice(i + 1, 1);
-            operators.splice(i, 1);
-            break;
-        } else if (operator === '/') {
-            result /= operand;
-            operands[i] = String(result)
-            operands.splice(i + 1 , 1);
-            operators.splice(i, 1);
-            break;
-        }
-    }
-    return [operators, operands, result]
+		return String(evalSumSubst(operators, operands, result));
+	}
 }
 
-function evalSumSubst(operators, operands, result){
-    for (let i = 0; i < operators.length; i++) {
-        const operator = operators[i];
-        result = parseFloat(operands[i]);
-        const operand = parseFloat(operands[i + 1]);
-      
-        if (operator === '+') {
-          result += operand;
-        } else if (operator === '-') {
-          result -= operand;
-        }
-    }
+function evalMultDiv(operators, operands, result) {
+	for (let i = 0; i < operators.length; i++) {
+		result = parseFloat(operands[i]);
+		const operator = operators[i];
+		const operand = parseFloat(operands[i + 1]);
 
-    return result
+		if (operator === "*") {
+			result *= operand;
+			operands[i] = String(result);
+			operands.splice(i + 1, 1);
+			operators.splice(i, 1);
+			break;
+		} else if (operator === "/") {
+			result /= operand;
+			operands[i] = String(result);
+			operands.splice(i + 1, 1);
+			operators.splice(i, 1);
+			break;
+		}
+	}
+	return [operators, operands, result];
+}
+
+function evalSumSubst(operators, operands, result) {
+	for (let i = 0; i < operators.length; i++) {
+		const operator = operators[i];
+		result = parseFloat(operands[i]);
+		const operand = parseFloat(operands[i + 1]);
+
+		if (operator === "+") {
+			result += operand;
+		} else if (operator === "-") {
+			result -= operand;
+		}
+	}
+
+	return result;
 }
 
 export default evalOperation;
